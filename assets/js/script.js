9d1879041abe59446072c4a8e0dd0e666d8d1af8
johnsiefert <johnsief12@gmail.com>
2021-10-18 13:17:32 -0400

    optimitized javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,25 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 let createTaskHandler = function(event){
    
-    console.log(event);
+     event.preventDefault();
 
-    event.preventDefault();
+let taskNameInput = document.querySelector("input[name='task-name']").value;
+let taskTypeInput = document.querySelector("select[name='task-type']").value;
+ // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
 
-    let listItemEl = document.createElement("li")
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// give it a class name
+taskInfoEl.className = "task-info";
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
 
+listItemEl.appendChild(taskInfoEl);
 
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
